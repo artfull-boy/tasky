9ffe6e1447b058fe44a029327ca0b3d41b5bbe90
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2024-11-18 18:08:39 +0000

    Create const.jsx

---
+++ src/lib/const.jsx
@@ -0,0 +1,32 @@
+export const PROJECT_STATUS_CLASS_MAP = {
+  pending: "bg-[#FEFCE8] text-[#A16207]",
+  in_progress: "bg-[#EFF6FF] text-[#2563EB]",
+  completed: "text-[#22C55E] bg-[#052E16] ",
+  };
+  export const PROJECT_STATUS_TEXT_MAP = {
+    pending: "Pending",
+    in_progress: "In Progress",
+    completed: "Completed",
+  };
+  export const TASK_STATUS_CLASS_MAP = {
+    pending: "bg-[#FEFCE8] text-[#A16207]",
+    in_progress: "bg-[#EFF6FF] text-[#2563EB]",
+    completed: "text-[#22C55E] bg-[#052E16] ",
+  };
+  
+  export const TASK_STATUS_TEXT_MAP = {
+    pending: "Pending",
+    in_progress: "In Progress",
+    completed: "Completed",
+  };
+  
+  export const TASK_PRIORITY_CLASS_MAP = {
+    low: "bg-gray-600",
+    medium: "bg-amber-600",
+    high: "bg-red-600",
+  };
+  export const TASK_PRIORITY_TEXT_MAP = {
+    low: "Low",
+    medium: "Medium",
+    high: "High",
+  };
